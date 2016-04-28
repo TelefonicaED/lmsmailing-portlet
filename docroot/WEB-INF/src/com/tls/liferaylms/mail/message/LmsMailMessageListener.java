69521c5cc7b735099d8f17e2c554c4fc973e73ea
francisco.moyaangulo <francisco.moyaangulo@telefonica.com>
2016-04-28 17:02:59 +0200

    Reestructuración del código.

--- docroot/WEB-INF/src/com/tls/liferaylms/mail/message/LmsMailMessageListener.java
+++ docroot/WEB-INF/src/com/tls/liferaylms/mail/message/LmsMailMessageListener.java
@@ -11,14 +11,12 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 
 import com.liferay.counter.service.CounterLocalServiceUtil;
 import com.liferay.lms.model.Course;
 import com.liferay.lms.model.LmsPrefs;
 import com.liferay.lms.service.CourseLocalServiceUtil;
 import com.liferay.lms.service.LmsPrefsLocalServiceUtil;
-import com.liferay.lms.service.ModuleLocalServiceUtil;
 import com.liferay.mail.service.MailServiceUtil;
 import com.liferay.portal.kernel.dao.orm.CustomSQLParam;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
@@ -46,13 +44,12 @@ import com.liferay.util.mail.MailEngine;
 import com.tls.liferaylms.mail.model.AuditSendMails;
 import com.tls.liferaylms.mail.service.AuditSendMailsLocalServiceUtil;
 
+
 public class LmsMailMessageListener implements MessageListener {
 	private static Log log = LogFactoryUtil.getLog(LmsMailMessageListener.class);
 
 	@Override
 	public void receive(Message message) {
-		// TODO Auto-generated method stub
-
 		try {
 			doReceive(message);
 		} catch (Exception e) {
@@ -62,41 +59,35 @@ public class LmsMailMessageListener implements MessageListener {
 
 	protected void doReceive(Message message) throws Exception {
 		_log.debug("LmsMailMessageListener doReceive");
+		
+		// Se recogen las variables.
 		String subject = message.getString("subject");
 		String body = message.getString("body");
 		long groupId = message.getLong("groupId");
 		long userId = message.getLong("userId");
 		String testing = message.getString("testing");
-		
 		String portal = message.getString("portal");
 		String community = message.getString("community");
-		
 		String url = message.getString("url");
 		String urlcourse = message.getString("urlcourse");
-		
 		String templateId = message.getString("templateId");
-		
 		String toMail = message.getString("to");
 		String userName = message.getString("userName");
 		boolean ownTeam = message.getBoolean("ownTeam");
 		boolean isOmniadmin = message.getBoolean("isOmniadmin");
 		
-;
-		
 		User sender = UserLocalServiceUtil.getUser(userId);
 		Group scopeGroup = GroupLocalServiceUtil.getGroup(groupId);
 		long companyId = scopeGroup.getCompanyId();
 		
-		String fromName = PrefsPropsUtil.getString(companyId,
-				PropsKeys.ADMIN_EMAIL_FROM_NAME);
-		String fromAddress = PrefsPropsUtil.getString(companyId,
-				PropsKeys.ADMIN_EMAIL_FROM_ADDRESS);
+		String fromName = PrefsPropsUtil.getString(companyId, PropsKeys.ADMIN_EMAIL_FROM_NAME);
+		String fromAddress = PrefsPropsUtil.getString(companyId, PropsKeys.ADMIN_EMAIL_FROM_ADDRESS);
 		
 		long nUsers = 0,millis = 0;
 		String numberUsers = PrefsPropsUtil.getString("lmsmailing.sendmails.number.users");
 		String milliseconds = PrefsPropsUtil.getString("lmsmailing.sendmails.wating.time.milliseconds");
 		
-		if(numberUsers != null && !numberUsers.equals("")){
+		if(Validator.isNotNull(numberUsers)){
 			nUsers = Long.valueOf(numberUsers);
 		}
 		
@@ -106,17 +97,16 @@ public class LmsMailMessageListener implements MessageListener {
 		
 		InternetAddress from = new InternetAddress(fromAddress, fromName);
 		
-		_log.debug("toMail: "+toMail+", userName: "+userName);
-		
 		if ("true".equals(testing)) {
-			if(_log.isDebugEnabled())_log.debug("Test");
-			InternetAddress to = new InternetAddress(sender.getEmailAddress(),
-					sender.getFullName());
+			if(_log.isDebugEnabled()) {
+				_log.debug("Se entra en modo testing");
+			}
+			InternetAddress to = new InternetAddress(sender.getEmailAddress(), sender.getFullName());
 			
-			body = createMessage(body, portal, community, sender.getFullName(), UserLocalServiceUtil.getUserById(userId).getFullName(),url,urlcourse);
+			body = createMessage(body, portal, community, sender.getFullName(), UserLocalServiceUtil.getUserById(userId).getFullName(), url, urlcourse);
 
 			String calculatedBody = LanguageUtil.get(Locale.getDefault(),"mail.header");
-			calculatedBody += createMessage(body, portal, community, sender.getFullName(), UserLocalServiceUtil.getUserById(userId).getFullName(),url,urlcourse);
+			calculatedBody += body;
 			calculatedBody += LanguageUtil.get(Locale.getDefault(),"mail.footer");
 			
 			subject = createMessage(subject, portal, community, sender.getFullName(), UserLocalServiceUtil.getUserById(userId).getFullName(),url,urlcourse);
@@ -124,47 +114,45 @@ public class LmsMailMessageListener implements MessageListener {
 			MailMessage mailm = new MailMessage(from, to, subject, calculatedBody, true);
 			MailServiceUtil.sendEmail(mailm);
 		} 
-		else if(toMail != null && userName != null && !toMail.equals("all")){
-			if(_log.isDebugEnabled())_log.debug("User");
+		else if(toMail != null && userName != null && !toMail.equals("all")) {
+			if(_log.isDebugEnabled()) {
+				_log.debug("Se entra en el envío individual de correos.");
+			}
 			User userSender = UserLocalServiceUtil.getUserById(userId);
-			User user = UserLocalServiceUtil.getUserByEmailAddress(userSender.getCompanyId(), toMail);
-			if(user!=null && user.isActive()){
+			User user = UserLocalServiceUtil.fetchUserByEmailAddress(userSender.getCompanyId(), toMail);
+			if(user != null && user.isActive()) {
 				InternetAddress to = new InternetAddress(toMail, userName);
 
-				if(_log.isDebugEnabled())_log.debug("Language::"+user.getLocale());
 				String calculatedBody = LanguageUtil.get(user.getLocale(),"mail.header");
-				calculatedBody += createMessage(body, portal, community, userName, UserLocalServiceUtil.getUserById(userId).getFullName(),url,urlcourse);
+				calculatedBody += createMessage(body, portal, community, userName, user.getFullName(),url,urlcourse);
 				calculatedBody += LanguageUtil.get(user.getLocale(),"mail.footer");
 				
-				String calculatedsubject = createMessage(subject, portal, community, userName, UserLocalServiceUtil.getUserById(userId).getFullName(),url,urlcourse);
+				String calculatedsubject = createMessage(subject, portal, community, userName, user.getFullName(),url,urlcourse);
 				
-				if(log.isDebugEnabled()){
-					log.debug("\n----------------------");
-					log.debug(" from: "+from);
-					log.debug(" to: "+toMail + " "+userName);
-					log.debug(" subject: "+calculatedsubject);
-					log.debug(" body: \n"+calculatedBody);
-					log.debug("----------------------");
+				if(log.isDebugEnabled()) {
+					log.debug("Se envía el siguiente correo...");
+					log.debug("De: " + from);
+					log.debug("A: " + toMail + " " + userName);
+					log.debug("Asunto: " + calculatedsubject);
+					log.debug("Cuerpo: " + calculatedBody);
 				}
 						
-				
 				MailMessage mailm = new MailMessage(from, to, calculatedsubject, calculatedBody ,true);
 				MailEngine.send(mailm);
 			}
 		}
-		else if(toMail.equals("all"))
-		{
-			_log.info("All-Start:"+Long.toString(groupId)+":"+scopeGroup.getName());
-			if(_log.isDebugEnabled())_log.debug("ownTeam: "+ownTeam);			
-			java.util.List<User> users = new ArrayList<User>();
-			if (ownTeam){
+		else if(toMail.equals("all")) {
+			if(_log.isDebugEnabled()) {
+				_log.debug("Se envía un correo a todos..." + ownTeam);
+			}
+			List<User> users = new ArrayList<User>();
+			if (ownTeam) {
 				List<Team> userTeams = TeamLocalServiceUtil.getUserTeams(userId);
 				Course course = CourseLocalServiceUtil.getCourseByGroupCreatedId(groupId);
-				LmsPrefs prefs=LmsPrefsLocalServiceUtil.getLmsPrefs(companyId);
+				LmsPrefs prefs = LmsPrefsLocalServiceUtil.getLmsPrefs(companyId);
 				
 				OrderByComparator obc = new   UserLastNameComparator(true);			
 				LinkedHashMap userParams = new LinkedHashMap();
-				int userCount = 0;
 
 				if (Validator.isNotNull(course)){
 					userParams.put("notInCourseRoleTeach", new CustomSQLParam("WHERE User_.userId NOT IN "
@@ -180,10 +168,9 @@ public class LmsMailMessageListener implements MessageListener {
 				              RoleLocalServiceUtil.getRole(prefs.getEditorRole()).getRoleId() }));
 				  	
 				  	if (ownTeam && !isOmniadmin && (userTeams!=null) && (userTeams.size()>0)){
-				  		
 				  		StringBuffer teamIds = new StringBuffer();
 				  		teamIds.append(userTeams.get(0).getTeamId());
-				  		if (userTeams.size() > 1){
+				  		if (userTeams.size() > 1) {
 					  		for(int i = 1; i<userTeams.size(); i++){
 					  			teamIds.append(",");
 					  			teamIds.append(userTeams.get(i).getTeamId());
@@ -191,7 +178,7 @@ public class LmsMailMessageListener implements MessageListener {
 				  		}
 				  		
 				  		userParams.put("inMyTeams", new CustomSQLParam("WHERE User_.userId IN "
-					              + " (SELECT distinct(Users_Teams.userId) FROM Users_Teams WHERE Users_Teams.teamId in ("+teamIds.toString()+ "))",null ));
+				  				+ " (SELECT distinct(Users_Teams.userId) FROM Users_Teams WHERE Users_Teams.teamId in ("+teamIds.toString()+ "))",null ));
 				  	}
 				  	
 				  	userParams.put("usersGroups", groupId);
@@ -199,106 +186,85 @@ public class LmsMailMessageListener implements MessageListener {
 				
 				users  = UserLocalServiceUtil.search(companyId, null, 0, userParams, QueryUtil.ALL_POS, QueryUtil.ALL_POS, obc);
 				
-			}else {
-				
+			}
+			else {
 				users = UserLocalServiceUtil.getGroupUsers(groupId);
-				_log.debug("Group users ::  "+users.size());
+			}
+			
+			if(_log.isDebugEnabled()) {
+				_log.debug("Se envía a " + users.size() + " usuarios.");
 			}
 			
 			int sendMails = 0;
-			long totalMails=0;
-			Session session=MailEngine.getSession();
-			boolean smtpAuth = GetterUtil.getBoolean(
-					_getSMTPProperty(session, "auth"), false);
-				String smtpHost = _getSMTPProperty(session, "host");
-				int smtpPort = GetterUtil.getInteger(
-					_getSMTPProperty(session, "port"), Account.PORT_SMTP);
-				String smtpuser = _getSMTPProperty(session, "user");
-				String password = _getSMTPProperty(session, "password");
-				Transport transport=null;
-				String protocol = GetterUtil.getString(
-						session.getProperty("mail.transport.protocol"),
-						Account.PROTOCOL_SMTP);
+			Session session = MailEngine.getSession();
+			String smtpHost = _getSMTPProperty(session, "host");
+			int smtpPort = GetterUtil.getInteger(_getSMTPProperty(session, "port"), Account.PORT_SMTP);
+			String smtpuser = _getSMTPProperty(session, "user");
+			String password = _getSMTPProperty(session, "password");
+			Transport transport = null;
+			String protocol = GetterUtil.getString(session.getProperty("mail.transport.protocol"), Account.PROTOCOL_SMTP);
 
-					transport = session.getTransport(protocol);
-				if (smtpAuth && Validator.isNotNull(smtpuser) &&
-					Validator.isNotNull(password)) {
-
-				
-					if(_log.isDebugEnabled())_log.debug("Connecting to SMTP Server");
-					try
-					{
-						_log.debug("Connecting to SMTP Server:"+protocol+":"+smtpHost+":"+smtpPort+":"+smtpuser+":"+password);
-						transport.connect(smtpHost, smtpPort, smtpuser, password);
-						_log.debug("Connected to SMTP Server:"+protocol+":"+smtpHost+":"+smtpPort+":"+smtpuser+":"+password);
-						
-					}
-					catch(MessagingException me)
-					{
-						me.printStackTrace();
-						throw new Exception(me);
-					}
-				}
-				else
-				{
-					if(_log.isDebugEnabled())_log.debug("Connecting to SMTP Server");
-					try
-					{
-						_log.debug("Connecting to SMTP Server:"+protocol+":"+smtpHost+":"+smtpPort);
-						transport.connect(smtpHost, smtpPort, null, null);
-						
-						_log.debug("Connected to SMTP Server:"+protocol+":"+smtpHost+":"+smtpPort);
-						
-					}
-					catch(MessagingException me)
-					{
-						me.printStackTrace();
-						//throw new Exception(me);
-					}
-				}
+			transport = session.getTransport(protocol);
+			if(_log.isDebugEnabled()) {
+				log.debug("Conectando con el servidor SMTP");
+				log.debug("Protocolo: " + protocol);
+				log.debug("Usuario: " + smtpuser);
+				log.debug("Contraseña: " + password);
+			}
+			
+			try {
+				transport.connect(smtpHost, smtpPort, smtpuser, password);
+				_log.debug("Conectado al servidor SMTP");
+			}
+			catch(MessagingException me) {
+				me.printStackTrace();
+				throw new Exception(me);
+			}
+			
+			User userSender = UserLocalServiceUtil.getUserById(userId);
+			String bodyTemplate = createTemplateMessage(body, portal, community, userSender.getFullName(), url, urlcourse);
+			String subjectTemplate = createTemplateMessage(subject, portal, community, userSender.getFullName(), url, urlcourse);
+			String calculatedBody, calculatedSubject;
+			
+			// Se envían los correos a todos los alumnos.
 			for (User user : users) {
-				if(user.isActive()){
-					if(nUsers > 0 && sendMails == nUsers ){
+				if (user.isActive()) {
+					if (nUsers > 0 && sendMails == nUsers) {
 						try {
-							if(_log.isDebugEnabled())_log.debug(" Delay " + millis +" milliseconds. Users: "+nUsers);
+							if(_log.isDebugEnabled())
+								_log.debug("Se ha llegado al número máximo de envíos en el bloque, se para " + millis + " milisegundos.");
 						    Thread.sleep(millis);
-						} catch(InterruptedException ex) {
+						}
+						catch(InterruptedException ex) {
 						    Thread.currentThread().interrupt();
 						}
-						
 						sendMails = 0;
-						
 					}
 					
-					
-					try{
-						
+					try {
 						InternetAddress to = new InternetAddress(user.getEmailAddress(), user.getFullName());
-						totalMails++;
-						if(_log.isDebugEnabled())
-						{
-							_log.debug("User::"+user.getEmailAddress()+"  number:"+Long.toString(totalMails));
-						
+						if(_log.isDebugEnabled()) {
+							_log.debug("Se envía un correo electrónico al siguiente usuario: " + user.getEmailAddress());
 						}
-						String calculatedBody = LanguageUtil.get(user.getLocale(),"mail.header");
-						calculatedBody += createMessage(body, portal, community, user.getFullName(), UserLocalServiceUtil.getUserById(userId).getFullName(),url,urlcourse);
+						
+						calculatedSubject = createMessage(subjectTemplate, user.getFullName());
+						
+						calculatedBody = LanguageUtil.get(user.getLocale(),"mail.header");
+						calculatedBody += createMessage(bodyTemplate, user.getFullName());
 						calculatedBody += LanguageUtil.get(user.getLocale(),"mail.footer");
-		
-						String calculatedsubject = createMessage(subject, portal, community, user.getFullName(), UserLocalServiceUtil.getUserById(userId).getFullName(),url,urlcourse);
-
-						if(log.isDebugEnabled()){
-							log.debug("\n----------------------");
-							log.debug(" from: "+from);
-							log.debug(" to: "+toMail + " "+userName);
-							log.debug(" subject: "+calculatedsubject);
-							log.debug(" body: \n"+calculatedBody);
-							log.debug("----------------------");
+						
+						if(log.isDebugEnabled()) {
+							log.debug("Se envía el siguiente correo...");
+							log.debug("De: " + from);
+							log.debug("A: " + toMail + " " + userName);
+							log.debug("Asunto: " + calculatedSubject);
+							log.debug("Cuerpo: " + calculatedBody);
 						}
 						
-						MailMessage mailm = new MailMessage(from, to, calculatedsubject, calculatedBody ,true);
-						
+						MailMessage mailm = new MailMessage(from, to, calculatedSubject, calculatedBody ,true);
 						sendMail(mailm,transport,session);
-					}catch(Exception meEx){
+					}
+					catch(Exception meEx){
 						meEx.printStackTrace();
 					}
 					sendMails++;
@@ -306,7 +272,7 @@ public class LmsMailMessageListener implements MessageListener {
 			}
 			transport.close();
 			
-			_log.info("All-End:"+Long.toString(groupId)+":"+scopeGroup.getName()+": Total Mails:"+totalMails);
+			_log.debug("Se finaliza el envío de correos electrónicos");
 			
 		}
 		
@@ -320,39 +286,60 @@ public class LmsMailMessageListener implements MessageListener {
 
 	}
 		
-		private static String _getSMTPProperty(Session session, String suffix) {
-			String protocol = GetterUtil.getString(
-				session.getProperty("mail.transport.protocol"));
+	private static String _getSMTPProperty(Session session, String suffix) {
+		String protocol = GetterUtil.getString(
+			session.getProperty("mail.transport.protocol"));
 
-			if (protocol.equals(Account.PROTOCOL_SMTPS)) {
-				return session.getProperty("mail.smtps." + suffix);
-			}
-			else {
-				return session.getProperty("mail.smtp." + suffix);
-			}
+		if (protocol.equals(Account.PROTOCOL_SMTPS)) {
+			return session.getProperty("mail.smtps." + suffix);
 		}
+		else {
+			return session.getProperty("mail.smtp." + suffix);
+		}
+	}
 	
-	private void sendMail(MailMessage mailm, Transport transport,Session session) throws MessagingException 
-	{
+	private void sendMail(MailMessage mailm, Transport transport,Session session) throws MessagingException {
 		javax.mail.Message message = new MimeMessage(session);
 		message.setFrom(mailm.getFrom());
 		message.setRecipients(javax.mail.Message.RecipientType.TO,mailm.getTo());
 		message.setSubject(mailm.getSubject());
-		MimeMultipart messageMultipart = new MimeMultipart(
-				"alternative");
 		if (mailm.isHTMLFormat()) {
-		
 			message.setContent(mailm.getBody(), "text/html;charset=\"UTF-8\"");
-
-			
 		}
 		else {
-		
 			message.setContent(mailm.getBody(),"text/plain;charset=\"UTF-8\"");
-
 		}
 		transport.sendMessage(message, mailm.getTo());
 		
+	}
+	
+	/*
+	 * Método que crea una plantilla de mensaje enviado para evitar tantos replaces. Se sustituyen todas las variables excepto las relacionadas con el usuario.
+	 */
+	private String createTemplateMessage (String text, String portal, String community, String teacher, String url, String urlcourse){
+		String res = "";
+		res = text.replace("[@portal]", 	portal);
+		res = res.replace ("[@course]", 	community);
+		res = res.replace ("[@teacher]", 	teacher);
+		res = res.replace ("[@url]", 		"<a href=\""+url+"\">"+portal+"</a>");
+		res = res.replace ("[@urlcourse]", 	"<a href=\""+urlcourse+"\">"+community+"</a>");	
+
+		//Se cambiala URL des.
+		res = changeToURL(res, url);
+		
+		return res;
+	}
+	
+	/*
+	 * Método que cambia cambia el nombre del usuario de la plantilla.
+	 */
+	private String createMessage(String text, String student) {
+		if(text != null) {
+			return text.replace ("[@student]", 	student);
+		}
+		else {
+			return "";
+		}
 	}
 
 	private String createMessage(String text, String portal, String community, String student, String teacher, String url, String urlcourse){
