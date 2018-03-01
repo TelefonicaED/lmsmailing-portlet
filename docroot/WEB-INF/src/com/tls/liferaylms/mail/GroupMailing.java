269390e13443de0c3ff7a5c743901fee94c66bd4
Jorge Rodríguez Fernández <jorge.rodriguezfernandez.ext@telefonicaed.com>
2018-03-01 12:00:15 +0100

    130929: Cuando envías un mensaje de prueba, que sólo se envíe una vez al creador, no una vez por cada usuario al que se lo ibas a mandar.

--- docroot/WEB-INF/src/com/tls/liferaylms/mail/GroupMailing.java
+++ docroot/WEB-INF/src/com/tls/liferaylms/mail/GroupMailing.java
@@ -124,6 +124,7 @@ public class GroupMailing extends MVCPortlet{
 		String body 	= ParamUtil.getString(actionRequest, "body", "");
 
 		String testing 	= ParamUtil.getString(actionRequest, "testing", "false");
+		boolean testMessage = testing.equals(StringPool.TRUE);
 		
 		String to 	= ParamUtil.getString(actionRequest, "to", "");
 		long entryId = -1;
@@ -148,27 +149,15 @@ public class GroupMailing extends MVCPortlet{
 			}
 		}
 		
-		
-		if(to.isEmpty()){
+		if(testMessage){
+			if (_log.isDebugEnabled()) _log.debug("Testing Mode: " + themeDisplay.getUser().getEmailAddress());
 			
-			if (_log.isDebugEnabled()) _log.debug("Enviamos a todos los usuarios");
-			
-			PermissionChecker permissionChecker = themeDisplay.getPermissionChecker();
-			PortletPreferences preferences = null;
-			String portletResource = ParamUtil.getString(actionRequest, "portletResource");
-			if (Validator.isNotNull(portletResource)) {
-				preferences = PortletPreferencesFactoryUtil.getPortletSetup(actionRequest, portletResource);
-			}else{
-				preferences = actionRequest.getPreferences();
-			}
-			boolean ownTeam = (preferences.getValue("ownTeam", "false")).compareTo("true") == 0;
-
 			Message message=new Message();
+			
 			message.put("templateId",-1);
-			message.put("to", "all");
-			message.put("ownTeam", ownTeam);
-			message.put("isOmniadmin", permissionChecker.isOmniadmin());
 			message.put("entryId", entryId);
+			message.put("to", themeDisplay.getUser().getEmailAddress());
+			message.put("userName", themeDisplay.getUser().getFullName());
 			message.put("subject", 	subject);
 			message.put("body", 	changeToURL(body, themeDisplay.getURLPortal()));
 			message.put("groupId", 	themeDisplay.getScopeGroupId());
@@ -180,25 +169,28 @@ public class GroupMailing extends MVCPortlet{
 			
 			message.put("url", 		themeDisplay.getURLPortal());
 			message.put("urlcourse",themeDisplay.getURLPortal()+themeDisplay.getPathFriendlyURLPublic()+themeDisplay.getScopeGroup().getFriendlyURL());
-			
 			MessageBusUtil.sendMessage("lms/mailing", message);
-		}else if (to.contains("team_")){
-			
-			String teamId = to.replace("team_", "");
-			if (_log.isDebugEnabled()) _log.debug("Enviamos al grupo de usuarios " + teamId);
-			LinkedHashMap<String, Object> userParams = new LinkedHashMap<String, Object>();
-			userParams.put("usersTeams", Long.parseLong(teamId));
-			OrderByComparator obc = new UserFirstNameComparator(true);
-			List<User> teamUsers = UserLocalServiceUtil.search(themeDisplay.getCompanyId(), "", 0, userParams, QueryUtil.ALL_POS, QueryUtil.ALL_POS, obc);	
-			for (User user : teamUsers) {
-				if (_log.isDebugEnabled()) _log.debug("user: " + user.getEmailAddress());
+		}else{
+			if(to.isEmpty()){
 				
+				if (_log.isDebugEnabled()) _log.debug("Enviamos a todos los usuarios");
+				
+				PermissionChecker permissionChecker = themeDisplay.getPermissionChecker();
+				PortletPreferences preferences = null;
+				String portletResource = ParamUtil.getString(actionRequest, "portletResource");
+				if (Validator.isNotNull(portletResource)) {
+					preferences = PortletPreferencesFactoryUtil.getPortletSetup(actionRequest, portletResource);
+				}else{
+					preferences = actionRequest.getPreferences();
+				}
+				boolean ownTeam = (preferences.getValue("ownTeam", "false")).compareTo("true") == 0;
+
 				Message message=new Message();
-				
 				message.put("templateId",-1);
+				message.put("to", "all");
+				message.put("ownTeam", ownTeam);
+				message.put("isOmniadmin", permissionChecker.isOmniadmin());
 				message.put("entryId", entryId);
-				message.put("to", user.getEmailAddress());
-				message.put("userName", user.getFullName());
 				message.put("subject", 	subject);
 				message.put("body", 	changeToURL(body, themeDisplay.getURLPortal()));
 				message.put("groupId", 	themeDisplay.getScopeGroupId());
@@ -210,19 +202,17 @@ public class GroupMailing extends MVCPortlet{
 				
 				message.put("url", 		themeDisplay.getURLPortal());
 				message.put("urlcourse",themeDisplay.getURLPortal()+themeDisplay.getPathFriendlyURLPublic()+themeDisplay.getScopeGroup().getFriendlyURL());
+				
 				MessageBusUtil.sendMessage("lms/mailing", message);
-			}
-		}else {
-			
-			if (_log.isDebugEnabled()) _log.debug("Enviamos a los usuarios seleccionados");
-			
-			String userIds[] = to.split(",");
-			
-			for(String id:userIds){
-
-				if(!id.trim().isEmpty()){
-					
-					User user = UserLocalServiceUtil.getUser(Long.valueOf(id));
+			}else if (to.contains("team_")){
+				
+				String teamId = to.replace("team_", "");
+				if (_log.isDebugEnabled()) _log.debug("Enviamos al grupo de usuarios " + teamId);
+				LinkedHashMap<String, Object> userParams = new LinkedHashMap<String, Object>();
+				userParams.put("usersTeams", Long.parseLong(teamId));
+				OrderByComparator obc = new UserFirstNameComparator(true);
+				List<User> teamUsers = UserLocalServiceUtil.search(themeDisplay.getCompanyId(), "", 0, userParams, QueryUtil.ALL_POS, QueryUtil.ALL_POS, obc);	
+				for (User user : teamUsers) {
 					if (_log.isDebugEnabled()) _log.debug("user: " + user.getEmailAddress());
 					
 					Message message=new Message();
@@ -236,20 +226,54 @@ public class GroupMailing extends MVCPortlet{
 					message.put("groupId", 	themeDisplay.getScopeGroupId());
 					message.put("userId",  	themeDisplay.getUserId());
 					message.put("testing", 	testing);
-
+					
 					message.put("portal", 	themeDisplay.getCompany().getName());
 					message.put("community",themeDisplay.getScopeGroupName());
 					
 					message.put("url", 		themeDisplay.getURLPortal());
 					message.put("urlcourse",themeDisplay.getURLPortal()+themeDisplay.getPathFriendlyURLPublic()+themeDisplay.getScopeGroup().getFriendlyURL());
-						
 					MessageBusUtil.sendMessage("lms/mailing", message);
+				}
+			}else {
+				
+				if (_log.isDebugEnabled()) _log.debug("Enviamos a los usuarios seleccionados");
+				
+				String userIds[] = to.split(",");
+				
+				for(String id:userIds){
+
+					if(!id.trim().isEmpty()){
+						
+						User user = UserLocalServiceUtil.getUser(Long.valueOf(id));
+						if (_log.isDebugEnabled()) _log.debug("user: " + user.getEmailAddress());
+						
+						Message message=new Message();
+						
+						message.put("templateId",-1);
+						message.put("entryId", entryId);
+						message.put("to", user.getEmailAddress());
+						message.put("userName", user.getFullName());
+						message.put("subject", 	subject);
+						message.put("body", 	changeToURL(body, themeDisplay.getURLPortal()));
+						message.put("groupId", 	themeDisplay.getScopeGroupId());
+						message.put("userId",  	themeDisplay.getUserId());
+						message.put("testing", 	testing);
+
+						message.put("portal", 	themeDisplay.getCompany().getName());
+						message.put("community",themeDisplay.getScopeGroupName());
+						
+						message.put("url", 		themeDisplay.getURLPortal());
+						message.put("urlcourse",themeDisplay.getURLPortal()+themeDisplay.getPathFriendlyURLPublic()+themeDisplay.getScopeGroup().getFriendlyURL());
+							
+						MessageBusUtil.sendMessage("lms/mailing", message);
+						
+					}
 					
 				}
 				
 			}
-			
 		}
+		
 	
 		if(_log.isInfoEnabled()){
 			_log.trace("ManageTemplates: sendNewMail\nTo: "+to+"\nSubject:\n" + subject +"\nBody:\n"+body);
